9a349a748db870a014feb52ab8a6b142c30e7689
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-22 14:00:31 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,123 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+const after = document.querySelector('#after')
+const better = document.querySelector('#better')
+const doIt = document.querySelector('#doIt')
+const ever = document.querySelector('#ever')
+const faster = document.querySelector('#faster')
+const harder = document.querySelector('#harder')
+const hour = document.querySelector('#hour')
+const makeIt = document.querySelector('#makeIt')
+const makesUs = document.querySelector('#makesUs')
+const moreThan = document.querySelector('#moreThan')
+const never = document.querySelector('#never')
+const our = document.querySelector('#our')
+const over = document.querySelector('#over')
+const stronger = document.querySelector('#stronger')
+const workIs = document.querySelector('#workIs')
+const workIt = document.querySelector('#workIt')
+
+//after.src = 'audio/after.after'
+
+  const afterPlay = document.querySelector('#afterPlay')
+    afterPlay.addEventListener('click', () => {
+    after.play()
+      })
+
+  const betterPlay = document.querySelector('#betterPlay')
+    betterPlay.addEventListener('click', () => {
+    better.play()
+      })
+
+  const doItPlay = document.querySelector('#doItPlay')
+    doItPlay.addEventListener('click', () => {
+    doIt.play()
+      })
+
+  const everPlay = document.querySelector('#everPlay')
+    everPlay.addEventListener('click', () => {
+    ever.play()
+      })
+
+  const fasterPlay = document.querySelector('#fasterPlay')
+    fasterPlay.addEventListener('click', () => {
+    faster.play()
+      })
+
+  const harderPlay = document.querySelector('#harderPlay')
+    harderPlay.addEventListener('click', () => {
+    harder.play()
+      })
+
+  const hourPlay = document.querySelector('#hourPlay')
+    hourPlay.addEventListener('click', () => {
+    hour.play()
+      })
+
+  const makeItPlay = document.querySelector('#makeItPlay')
+    makeItPlay.addEventListener('click', () => {
+    makeIt.play()
+      })
+
+  const makeUsPlay = document.querySelector('#makeUsPlay')
+    makeUsPlay.addEventListener('click', () => {
+    makeUs.play()
+      })
+  const moreThanPlay = document.querySelector('#moreThanPlay')
+    moreThanPlay.addEventListener('click', () => {
+    moreThan.play()
+      })
+
+  const neverPlay = document.querySelector('#neverPlay')
+    neverPlay.addEventListener('click', () => {
+    never.play()
+      })
+
+  const ourPlay = document.querySelector('#ourPlay')
+    ourPlay.addEventListener('click', () => {
+    our.play()
+      })
+  const overPlay = document.querySelector('#overPlay')
+    overPlay.addEventListener('click', () => {
+    over.play()
+      })
+
+  const strongerPlay = document.querySelector('#strongerPlay')
+    strongerPlay.addEventListener('click', () => {
+    stronger.play()
+      })
+
+  const worIsPlay = document.querySelector('#worIsPlay')
+    worIsPlay.addEventListener('click', () => {
+    worIs.play()
+      })
+
+  const worItPlay = document.querySelector('#worItPlay')
+    worItPlay.addEventListener('click', () => {
+    worIt.play()
+      })
+
+
+
+
+
+
+
+
+
+
+//
+// play.addEventListener('mouseover'), [0]
+//     audio.currentTime = 0
+//     audio.play()
+//
+// pause.addEventListener('mousemove'), [0]
+//     audioPlayer.currentTime = 0
+//     audio.pause()
+
+
+
+//const workIt = document.querySelector('work_it')
+
+
+})
